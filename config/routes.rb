c1502e167547243084bad59b2a4f9d0e15702734
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-12 15:08:41 -0500

    Add update controller action and route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
 
-  resources :users, only: [:show, :index, :edit]
+  resources :users, only: [:show, :index, :edit, :update]
 end
