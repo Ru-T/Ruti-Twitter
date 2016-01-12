95dfee7ace93202e8be77c883fbd80603cf4c025
Ruti Wajnberg <rutweiler@Rutis-MacBook-Pro.local>
2016-01-12 13:43:58 -0500

    Add devise files.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+  end
+
+  def index
+    @users = User.all
+  end
+  
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # strong params
+  def user_params
+    params.require(:user).permit(:name, :bio)
+  end
+end
