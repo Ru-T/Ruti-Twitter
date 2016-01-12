669127a2f551a7846c0cc57067743c29c5ea889b
Ruti Wajnberg <rutweiler@Rutis-MacBook-Pro.local>
2016-01-12 11:47:02 -0500

    Add seed user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create!(name: "Ruti", bio: "I am the first user of this Twitter Clone app!")
