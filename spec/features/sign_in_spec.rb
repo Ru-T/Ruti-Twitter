a6351b2d1ac57528f236ebd6c00e4923eedffaa6
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-13 15:33:32 -0500

    Require capybara/rspec, not rails.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 feature 'User signs in' do
   
-  # before :each do
-  #   User.create(:email => 'newuser@sb.com', :password => 'password')
-  # end 
+  before :each do
+    User.create(:email => 'newuser@sb.com', :password => 'password')
+  end 
 
   scenario 'with valid credentials' do
     visit new_user_session_path
