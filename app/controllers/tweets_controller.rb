cf457d1df82e55ede186f717881d039c1740f2da
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-13 09:34:56 -0500

    Add path for new Tweet and link to from index view.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,17 @@ class TweetsController < ApplicationController
   end
 
   def new
-    @tweet = Tweet.new
+    @tweet = Tweet.new#(user: current_user)
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    if @tweet.save
+      redirect_to tweets_path
+    else
+      render :new
+    end
   end
 
   def index
