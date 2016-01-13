448e9ef43744f7406f3c48cdf26daa565d4de51e
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-13 14:01:03 -0500

    Add before actions for login/log out tests.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 feature 'User signs in' do
+  
+  before :each do
+    User.create(:email => 'newuser@sb.com', :password => 'password')
+  end 
+
   scenario 'with valid credentials' do
     visit new_user_session_path
-    fill_in 'Email', with: 'ruti@sb.com'
+    fill_in 'Email', with: 'newuser@sb.com'
     fill_in 'Password', with: 'password' 
     click_on 'Log In'
 
--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 feature 'User signs out' do
+
+  before :each do
+    User.create(:email => 'newuser2@sb.com', :password => 'password')
+  end 
+
   scenario 'sign out' do
     visit new_user_session_path
-    fill_in 'Email', with: 'ruti@sb.com'
+    fill_in 'Email', with: 'newuser2@sb.com'
     fill_in 'Password', with: 'password' 
     click_on 'Log In'
     click_on 'Log Out'
