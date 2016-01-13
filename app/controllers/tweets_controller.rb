85cac8d90b9232a293dda55abb80cb30dbbefc6f
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-13 09:44:35 -0500

    Auto assign user_id of logged in user when Tweet is created.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,17 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show]
+  before_action :authenticate_user!
 
   def show
   end
 
   def new
-    @tweet = Tweet.new#(user: current_user)
+    @tweet = Tweet.new
   end
 
   def create
     @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
 
     if @tweet.save
       redirect_to tweets_path
