e8c4efd86b53776674f527c0d4442b437f51d88a
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-14 17:14:10 -0500

    Add factory file for tweet.

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -3,16 +3,12 @@ require 'rails_helper'
 RSpec.describe Tweet, type: :model do
 
   describe '.default scope' do
-    tweet_two = FactoryGirl.create(:tweet)
-    tweet_one = FactoryGirl.create(:tweet)
-
     it 'orders by most recently posted' do
       expect(Tweet.all).to eq [tweet_one, tweet_two]
     end
   end  
 
   describe 'validations' do
-
     it "is valid with both user_id and text" do
       expect(FactoryGirl.build(:tweet)).to be_valid    
     end
@@ -24,7 +20,6 @@ RSpec.describe Tweet, type: :model do
     it "must have user_id to be valid" do
       expect(FactoryGirl.build(:tweet, text: "I am a tweet.", user_id: nil)).to_not be_valid
     end
-  
   end  
 
 
