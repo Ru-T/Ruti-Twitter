1d24e7090da114daa1f7ca15a9c87b5ddce6f87a
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-14 17:13:40 -0500

    Add tests for tweet validations presence true.

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,4 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
+
+  describe '.default scope' do
+    tweet_two = FactoryGirl.create(:tweet)
+    tweet_one = FactoryGirl.create(:tweet)
+
+    it 'orders by most recently posted' do
+      expect(Tweet.all).to eq [tweet_one, tweet_two]
+    end
+  end  
+
+  describe 'validations' do
+
+    it "is valid with both user_id and text" do
+      expect(FactoryGirl.build(:tweet)).to be_valid    
+    end
+
+    it "must have text to be valid" do
+      expect(FactoryGirl.build(:tweet, user_id: 1, text: "")).to_not be_valid
+    end
+
+    it "must have user_id to be valid" do
+      expect(FactoryGirl.build(:tweet, text: "I am a tweet.", user_id: nil)).to_not be_valid
+    end
+  
+  end  
+
+
+
+
 end
