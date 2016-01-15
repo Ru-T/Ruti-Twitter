9c6109540039e8142129c34c6ce8d292c4a29e60
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-15 09:35:49 -0500

    Add validations with tests on follow.

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Follow, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it "is valid with both followed_id and follower_id" do
+      expect(FactoryGirl.build(:follow)).to be_valid
+    end
+
+    it "must have both ids to be valid" do
+      expect(FactoryGirl.build(:follow, follower_id: 1, followed_id: nil)).to_not be_valid
+    end
+
+    it "must have both ids to be valid" do
+      expect(FactoryGirl.build(:follow, followed_id: 1, follower_id: nil)).to_not be_valid
+    end
+  end
 end
