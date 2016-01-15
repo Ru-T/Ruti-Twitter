4eb42f0f71fbd52bd0587ec4b5551204be7d82cb
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-15 10:24:35 -0500

    Move Tweet seed data into using Faker.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 User.create!(email: "ruti@sb.com", password: "password", name: "Ruti", bio: "I am the first user of this Twitter Clone app!")
 
 Tweet.create!(user_id: 1, text: "This is a Tweet!")
+
+10.times do
+  Tweet.create!(user_id: 1, text: Faker::Lorem.characters(100))
+end
