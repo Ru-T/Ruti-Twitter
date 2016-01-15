f6758c050d5aa63158e2f25251e6433c45ff5c41
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-15 17:06:48 -0500

    Change syntax of adding image in user factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,11 @@
+include ActionDispatch::TestProcess
+
 FactoryGirl.define do
   factory :user do |user|
     user.email "newuser@sb.com"
     user.password "password"
     user.name "NewUser"
     user.bio "This is my bio."
-    # user.avatar {fixture_file_upload 'Screen_Shot.png', 'image/png'}
+    user.avatar { Rack::Test::UploadedFile.new(File.join(Rails.root, 'public', 'uploads', 'user', 'avatar', '1', 'Screen_Shot.png')) }
   end
 end
