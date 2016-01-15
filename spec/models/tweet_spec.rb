2eb4945d29892e7dc21f6fe08c36cfcf8763cfbc
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-15 10:31:08 -0500

    Removed unnecessary params from unit tests.

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -7,24 +7,24 @@ RSpec.describe Tweet, type: :model do
       tweet_one = FactoryGirl.create(:tweet)
       expect(Tweet.first).to eq tweet_one
     end
-  end  
+  end
 
   describe 'validations' do
     it "is valid with both user_id and text" do
-      expect(FactoryGirl.build(:tweet)).to be_valid    
+      expect(FactoryGirl.build(:tweet)).to be_valid
     end
 
     it "must have text to be valid" do
-      expect(FactoryGirl.build(:tweet, user_id: 1, text: "")).to_not be_valid
+      expect(FactoryGirl.build(:tweet, text: "")).to_not be_valid
     end
 
     it "must have user_id to be valid" do
-      expect(FactoryGirl.build(:tweet, text: "I am a tweet.", user_id: nil)).to_not be_valid
+      expect(FactoryGirl.build(:tweet, user_id: nil)).to_not be_valid
     end
 
     it "must be under 140 characters" do
-      expect(FactoryGirl.build(:tweet, text: "a" * 141, user_id: 1)).to_not be_valid
+      expect(FactoryGirl.build(:tweet, text: "a" * 141)).to_not be_valid
     end
-  end  
+  end
 
 end
