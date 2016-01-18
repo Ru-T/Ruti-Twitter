7da0e55228358cffc589273cb09179858cbb9fe6
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-18 15:32:33 -0500

    Modify tweet specs to use let instead of FactoryGirl inside it statements.

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
 
+  let(:tweet) {FactoryGirl.create(:tweet)}
+  let(:tweet_one) {create(:tweet)}
+
   describe '.default scope' do
     it 'orders by most recently posted' do
       tweet_two = FactoryGirl.create(:tweet)
-      tweet_one = FactoryGirl.create(:tweet)
-      expect(Tweet.first).to eq tweet_one
+      expect(Tweet.first).to eq tweet_two
     end
   end
 
@@ -25,6 +27,21 @@ RSpec.describe Tweet, type: :model do
 
     it "must be under 140 characters" do
       expect(FactoryGirl.build(:tweet, text: "a" * 141)).to_not be_valid
+    end
+  end
+
+  describe '#favorite' do
+    it "favorites a tweet" do
+      tweet.favorite
+      expect(tweet.favorited?).to eq true
+    end
+  end
+
+  describe '#unfavorite' do
+    it "unfavorites a tweet" do
+      tweet.favorite
+      tweet.unfavorite
+      expect(tweet.favorited?).to eq false
     end
   end
 
