1a3bfa85fc00bb4c0f09dc07ce239390b1aba5ac
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-18 16:16:19 -0500

    Add tests on user for favorite methods.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
 
     let(:user1) {FactoryGirl.create(:user)}
     let(:user2) {create(:user, email: "shs@test.com")}
+    let(:tweet) {create(:tweet)}
 
     describe '#follow' do
       it "allows a user to follow another" do
@@ -26,6 +27,30 @@ RSpec.describe User, type: :model do
         expect(user1.following?(user2)).to eq true
         user1.unfollow(user2)
         expect(user1.following?(user2)).to eq false
+      end
+    end
+
+    describe '#favorite' do
+      it "allows a user to favorite a tweet" do
+        user1.favorite(tweet)
+        expect(user1.favorite_tweets).to include tweet
+      end
+    end
+
+    describe '#unfavorite' do
+      it "allows a user to unfavorite a tweet" do
+        user1.favorite(tweet)
+        user1.unfavorite(tweet)
+        expect(user1.favorite_tweets).to_not include tweet
+      end
+    end
+
+    describe '#favorited?' do
+      it "checks if a tweet has been favorited by a user" do
+        user1.favorite(tweet)
+        expect(user1.favorited?(tweet)).to eq true
+        user1.unfavorite(tweet)
+        expect(user1.favorited?(tweet)).to eq false
       end
     end
 end
