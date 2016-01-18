b16ba1431baee34a41d409c5bba3db44b4054114
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-18 15:22:29 -0500

    Fix rspec unit test file for user to use let instead of before action.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,30 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-    describe '#follow and unfollow' do
+    let(:user1) {FactoryGirl.create(:user)}
+    let(:user2) {create(:user, email: "shs@test.com")}
 
-      before(:each) do
-        @user1 = FactoryGirl.create(:user)
-        @user2 = FactoryGirl.create(:user, email: "newuser2@sb.com")
-      end
-
+    describe '#follow' do
       it "allows a user to follow another" do
-        @user1.follow(@user2)
-        expect(@user1.following?(@user2)).to eq true
-        expect(@user2.followers).to include @user1
+        user1.follow(user2)
+        expect(user2.followers).to include user1
       end
+    end
 
+    describe '#unfollow' do
       it "allows a user to unfollow another" do
-        @user1.follow(@user2)
-        @user1.unfollow(@user2)
-        expect(@user1.following?(@user2)).to eq false
-        expect(@user2.followers).to_not include @user1
+        user1.follow(user2)
+        user1.unfollow(user2)
+        expect(user2.followers).to_not include user1
+      end
+    end
+
+    describe '#following' do
+      it "checks if a user is following another" do
+        user1.follow(user2)
+        expect(user1.following?(user2)).to eq true
+        user1.unfollow(user2)
+        expect(user1.following?(user2)).to eq false
       end
     end
 end
