a40a1c2014dce71d6c6bca4e2c151f91cc248611
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-18 14:53:45 -0500

    Move assignment of variables into before action in rspec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-    describe '#follow' do
-      it "allows a user to follow another" do
-        user1 = FactoryGirl.create(:user)
-        user2 = FactoryGirl.create(:user, email: "newuser2@sb.com")
-        user1.follow(user2)
-        expect(user1.following?(user2)).to eq true
-        expect(user2.followers).to include user1
-      end
-    end
+    describe '#follow and unfollow' do
 
-    describe '#unfollow' do
+      before(:each) do
+        @user1 = FactoryGirl.create(:user)
+        @user2 = FactoryGirl.create(:user, email: "newuser2@sb.com")
+      end
+
+      it "allows a user to follow another" do
+        @user1.follow(@user2)
+        expect(@user1.following?(@user2)).to eq true
+        expect(@user2.followers).to include @user1
+      end
+
       it "allows a user to unfollow another" do
-        user1 = FactoryGirl.create(:user)
-        user2 = FactoryGirl.create(:user, email: "newuser2@sb.com")
-        user1.follow(user2)
-        user1.unfollow(user2)
-        expect(user1.following?(user2)).to eq false
-        expect(user2.followers).to_not include user1
+        @user1.follow(@user2)
+        @user1.unfollow(@user2)
+        expect(@user1.following?(@user2)).to eq false
+        expect(@user2.followers).to_not include @user1
       end
     end
 end
