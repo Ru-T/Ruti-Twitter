49c64b81996bd6a669d04b953ed22a7cb63410c8
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-18 09:27:27 -0500

    Modify language for followed users to make more readable.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 
   has_many :tweets
   has_many :follows, foreign_key: "follower_id"
-  has_many :followeds, through: :follows
+  has_many :followed_users, through: :follows, source: :followed
 
   has_many :passive_follows, class_name: "Follow",
                           foreign_key: "followed_id"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
         user1 = FactoryGirl.create(:user)
         user2 = FactoryGirl.create(:user, email: "newuser2@sb.com")
         user1.follow(user2)
-        expect(user1.followeds).to include user2
+        expect(user1.followed_users).to include user2
         expect(user2.followers).to include user1
       end
     end
@@ -18,7 +18,7 @@ RSpec.describe User, type: :model do
         user2 = FactoryGirl.create(:user, email: "newuser2@sb.com")
         user1.follow(user2)
         user1.unfollow(user2)
-        expect(user1.followeds).to_not include user2
+        expect(user1.followed_users).to_not include user2
         expect(user2.followers).to_not include user1
       end
     end
