c461381580f0b72e85e80ab81cb41341828080cd
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-18 11:06:34 -0500

    Add nested route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#show'
 
-  resources :users, only: [:show, :index, :edit, :update, :create]
+  resources :users, only: [:show, :index, :edit, :update, :create] do
+    member do
+      get :following
+    end
+  end
   resources :tweets, only: [:create, :new]
   resources :follows, only: [:index, :create, :destroy]
 end
