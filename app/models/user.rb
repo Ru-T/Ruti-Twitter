358ddf6259b5e9b118faf766fd3781b2ea259541
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-18 09:25:30 -0500

    Pass unit tests for follow/unfollow methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,19 @@ class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
 
   has_many :tweets
-  has_many :active_follows, class_name: "Follow",
-                          foreign_key: "follower_id"
+  has_many :follows, foreign_key: "follower_id"
+  has_many :followeds, through: :follows
+
   has_many :passive_follows, class_name: "Follow",
                           foreign_key: "followed_id"
-  has_many :followeds, through: :active_follows
   has_many :followers, through: :passive_follows
 
   def follow(another_user)
-    Follow.create!(followed_id: another_user.id)
+    follows.create!(followed_id: another_user.id)
   end
 
   def unfollow(another_user)
-    Follow.find_by(followed_id: another_user.id).destroy!
+    follows.find_by_followed_id(another_user.id).destroy
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe User, type: :model do
 
     describe '#follow' do
       it "allows a user to follow another" do
-        user1 = FactoryGirl.build(:user)
-        user2 = FactoryGirl.build(:user)
+        user1 = FactoryGirl.create(:user)
+        user2 = FactoryGirl.create(:user, email: "newuser2@sb.com")
         user1.follow(user2)
         expect(user1.followeds).to include user2
         expect(user2.followers).to include user1
@@ -14,8 +14,8 @@ RSpec.describe User, type: :model do
 
     describe '#unfollow' do
       it "allows a user to unfollow another" do
-        user1 = FactoryGirl.build(:user)
-        user2 = FactoryGirl.build(:user)
+        user1 = FactoryGirl.create(:user)
+        user2 = FactoryGirl.create(:user, email: "newuser2@sb.com")
         user1.follow(user2)
         user1.unfollow(user2)
         expect(user1.followeds).to_not include user2
