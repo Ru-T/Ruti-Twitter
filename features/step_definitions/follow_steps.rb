f4f34d0344e822742f7b063924726dc6e58b0af8
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-19 15:38:56 -0500

    Pass follow and favorite integration tests.

--- features/step_definitions/follow_steps.rb
+++ features/step_definitions/follow_steps.rb
@@ -1,14 +1,13 @@
 When(/^I visit a user's profile$/) do
-  @user2 = FactoryGirl.create(:user, id: 2, email: "newuser2@sb.com")
-  visit user_path(@user2)
+  user2 = FactoryGirl.create(:user, id: 2, email: "newuser2@sb.com")
+  tweet = FactoryGirl.create(:tweet, user: user2, text: "This is a tweet!")
+  visit user_path(user2)
 end
 
 Then(/^I see my followed user's tweet$/) do
-  tweet = FactoryGirl.create(:tweet, user: @user2, text: "This is a tweet!")
-  expect(page).to have_content(tweet.text)
+  expect(page).to have_content("This is a tweet!")
 end
 
 Then(/^I no longer see my followed user's tweet$/) do
-  tweet = FactoryGirl.create(:tweet, user: @user2, text: "This is a tweet!")
-  expect(page).to_not have_content(tweet.text)
+  expect(page).to_not have_content("This is a tweet!")
 end
