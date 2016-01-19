ca02156516f31040275487c32adfd30d15a492ff
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-19 14:46:52 -0500

    Closer to working follow integration tests.

--- features/step_definitions/follow_steps.rb
+++ features/step_definitions/follow_steps.rb
@@ -1,14 +1,14 @@
 When(/^I visit a user's profile$/) do
   @user2 = FactoryGirl.create(:user, id: 2, email: "newuser2@sb.com")
-  visit "users/2"
+  visit user_path(@user2)
 end
 
 Then(/^I see my followed user's tweet$/) do
   tweet = FactoryGirl.create(:tweet, user: @user2, text: "This is a tweet!")
-  expect(page).to have_content(tweet)
+  expect(page).to have_content(tweet.text)
 end
 
 Then(/^I no longer see my followed user's tweet$/) do
   tweet = FactoryGirl.create(:tweet, user: @user2, text: "This is a tweet!")
-  expect(page).to_not have_content(tweet)
+  expect(page).to_not have_content(tweet.text)
 end
